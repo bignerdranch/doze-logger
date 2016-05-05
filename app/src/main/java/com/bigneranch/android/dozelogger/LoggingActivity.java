0eb91cbfefe37df8e664946839e5cb3644cd52db
paul <pturner@bignerdranch.com>
2016-05-05 11:02:11 -0400

    Initial project creation

---
+++ app/src/main/java/com/bigneranch/android/dozelogger/LoggingActivity.java
@@ -0,0 +1,13 @@
+package com.bigneranch.android.dozelogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoggingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logging);
+    }
+}
