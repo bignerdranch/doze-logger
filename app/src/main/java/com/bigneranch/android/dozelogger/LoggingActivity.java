ce1517a9b9b341f3ee8aa7eb4598164cb8cbdef2
paul <pturner@bignerdranch.com>
2016-05-06 16:05:27 -0400

    create recycler view that displays log messages

--- app/src/main/java/com/bigneranch/android/dozelogger/LoggingActivity.java
+++ app/src/main/java/com/bigneranch/android/dozelogger/LoggingActivity.java
@@ -1,13 +1,62 @@
 package com.bigneranch.android.dozelogger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.ViewGroup;
+
+import com.bigneranch.android.dozelogger.databinding.LogItemViewBinding;
+import com.bigneranch.android.dozelogger.databinding.LoggingActivityBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoggingActivity extends AppCompatActivity {
+
+    private List<String> logs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logging);
+        LoggingActivityBinding loggingActivityBinding = DataBindingUtil.setContentView(this, R.layout.logging_activity);
+        loggingActivityBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        logs = PermanentLoggerUtil.getLogs(this);
+        loggingActivityBinding.recyclerView.setAdapter(new LogAdapter());
+    }
+
+    private class LogAdapter extends Adapter<LogViewHolder> {
+
+        @Override
+        public LogViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LogItemViewBinding logItemViewBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.log_item_view, parent, false);
+            return new LogViewHolder(logItemViewBinding);
+        }
+
+        @Override
+        public void onBindViewHolder(LogViewHolder holder, int position) {
+            holder.bind(logs.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return logs.size();
+        }
+    }
+
+
+    private class LogViewHolder extends ViewHolder {
+        private LogItemViewBinding logItemViewBinding;
+
+        public LogViewHolder(LogItemViewBinding logItemViewBinding) {
+            super(logItemViewBinding.getRoot());
+            this.logItemViewBinding = logItemViewBinding;
+        }
+
+        public void bind(String text) {
+            logItemViewBinding.textView.setText(text);
+        }
     }
 }
--- app/src/main/java/com/bigneranch/android/dozelogger/PermanentLoggerUtil.java
+++ app/src/main/java/com/bigneranch/android/dozelogger/PermanentLoggerUtil.java
@@ -3,8 +3,15 @@ package com.bigneranch.android.dozelogger;
 import android.content.Context;
 import android.util.Log;
 
+import com.google.common.base.Strings;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PermanentLoggerUtil {
 
@@ -20,10 +27,29 @@ public class PermanentLoggerUtil {
         try {
             OutputStreamWriter logFileOutputStreamWriter = new OutputStreamWriter(context.openFileOutput(LOG_FILE, Context.MODE_APPEND));
             logFileOutputStreamWriter.write(message);
+            logFileOutputStreamWriter.write('\n');
             logFileOutputStreamWriter.close();
         } catch (IOException e) {
             Log.e(TAG, "Failed to append line to file", e);
         }
+    }
+
+    public static List<String> getLogs(Context context) {
+        List<String> logs = new ArrayList<String>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(context.openFileInput(LOG_FILE)));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                logs.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read file", e);
+        }
+
+        return logs;
     }
 
 }
